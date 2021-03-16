f6deabe31df251fc5c51b5652a0a44a432d99d6a
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-03-16 14:01:58 -0300

    StaticFiles
    Resolvendo a não importação de arquiivos estativos

--- ibbms1/settings.py
+++ ibbms1/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+
+]
 
 
 # django-allauth
