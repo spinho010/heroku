5161fd51df509bfaf786ff348d1bc0004e8408ac
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-03-24 13:41:13 -0300

    Retirando o modelo de usuario personalizado

--- users/admin.py
+++ users/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 
-from .forms import UserChangeForm, UserCreationForm
-from .models import User
+#from .forms import UserChangeForm, UserCreationForm
+#from .models import User
 from users.models import iuser
 
 
-@admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-    model = User
-    fieldsets = auth_admin.UserAdmin.fieldsets + (
-        ("Informações Pessoais", {"fields": ("nome_completo", "idade", "cargo", "situacao", "niver", "conversao", "batismo", "Localizacao", "telefone", "foto")}),
-    )
+#@admin.register(User)
+#class UserAdmin(auth_admin.UserAdmin):
+    #form = UserChangeForm
+    #add_form = UserCreationForm
+    #model = User
+    #fieldsets = auth_admin.UserAdmin.fieldsets + (
+        #("Informações Pessoais", {"fields": ("nome_completo", "idade", "cargo", "situacao", "niver", "conversao", "batismo", "Localizacao", "telefone", "foto")}),
+    #)
 
 
 class iuserUser(admin.ModelAdmin):
--- users/models.py
+++ users/models.py
@@ -1,21 +1,21 @@
-from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.models import AbstractUser
 from django.db import models
 from PIL import *
 from django.contrib.auth.models import User
 
 
 
-class User(AbstractUser):
-    nome_completo = models.CharField(verbose_name='Nome Completo:', max_length=60, blank=True)
-    idade = models.CharField(verbose_name='Idade:', max_length=15, blank=True)
-    cargo = models.CharField(verbose_name='Cargo:', max_length=100, blank=True)
-    situacao = models.CharField(verbose_name='Situação:', max_length=20, blank=True)
-    niver = models.CharField(verbose_name='Data de Aniversário:', max_length=30, blank=True)
-    conversao = models.CharField(verbose_name='Data de Conversão:', max_length=30, blank=True)
-    batismo = models.CharField(verbose_name='Data de Batismo:', max_length=30, blank=True)
-    Localizacao = models.CharField(verbose_name='Endereço:', max_length=200, blank=True)
-    telefone = models.CharField(verbose_name='Telefone:', max_length=30, blank=True)
-    foto = models.ImageField(verbose_name='Foto', upload_to='D:/', default='/static/midia/10.png')
+#class User(AbstractUser):
+    #nome_completo = models.CharField(verbose_name='Nome Completo:', max_length=60, blank=True)
+    #idade = models.CharField(verbose_name='Idade:', max_length=15, blank=True)
+    #cargo = models.CharField(verbose_name='Cargo:', max_length=100, blank=True)
+    #situacao = models.CharField(verbose_name='Situação:', max_length=20, blank=True)
+    #niver = models.CharField(verbose_name='Data de Aniversário:', max_length=30, blank=True)
+    #conversao = models.CharField(verbose_name='Data de Conversão:', max_length=30, blank=True)
+    #batismo = models.CharField(verbose_name='Data de Batismo:', max_length=30, blank=True)
+    #Localizacao = models.CharField(verbose_name='Endereço:', max_length=200, blank=True)
+    #telefone = models.CharField(verbose_name='Telefone:', max_length=30, blank=True)
+    #foto = models.ImageField(verbose_name='Foto', upload_to='D:/', default='/static/midia/10.png')
 
 
 
